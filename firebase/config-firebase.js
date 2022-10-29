205d585bc14b09d0c3b8892f97c07387807c0e5f
Cristian Developer <devfullstacknelson@gmail.com>
2022-10-29 15:35:39 -0300

    uploaded store firebase 2

--- firebase/config-firebase.js
+++ firebase/config-firebase.js
@@ -7,23 +7,13 @@ import { initializeApp } from "firebase/app";
 // devfullstacknelson@gmail.com
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDf_BPTRxJL0agVHqReUX5GFlKw2raadqc",
-  authDomain: "fayfaviposis.firebaseapp.com",
-  projectId: "fayfaviposis",
-  storageBucket: "gs://fayfaviposis.appspot.com",
-  messagingSenderId: "536830927116",
-  appId: "1:536830927116:web:1dad6c83dcf7b42fe8e929"
+  apiKey: "AIzaSyCyeDAO5MInKwH1fbv61mho_6Qdu3Kb3oE",
+  authDomain: "posis-fayfa-suc2.firebaseapp.com",
+  projectId: "posis-fayfa-suc2",
+  storageBucket: "gs://posis-fayfa-suc2.appspot.com",
+  messagingSenderId: "992441310532",
+  appId: "1:992441310532:web:ad30bf981cd48569af4b11"
 };
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCyeDAO5MInKwH1fbv61mho_6Qdu3Kb3oE",
-//   authDomain: "posis-fayfa-suc2.firebaseapp.com",
-//   projectId: "posis-fayfa-suc2",
-//   storageBucket: "gs://posis-fayfa-suc2.appspot.com",
-//   messagingSenderId: "992441310532",
-//   appId: "1:992441310532:web:ad30bf981cd48569af4b11"
-// };
 
 // Initialize Firebase
 export const firebase = () => initializeApp(firebaseConfig);
