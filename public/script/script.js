d7607014319b31b593fbc355fcdea45bba9898a9
Cristian Developer <devfullstacknelson@gmail.com>
2022-11-05 16:15:39 -0300

    funcionalidad descargar secuencia y actualizar/rehabilitar secuencia

--- public/script/script.js
+++ public/script/script.js
@@ -1,12 +1,62 @@
-function handleCloseMessage (closeIcon){
-      
-    document.querySelector('.message').classList.add('hidden');
- }
+function handleCloseMessage(closeIcon) {
+
+   document.querySelector('.message').classList.add('hidden');
+}
 
 function handleSendData() {
-    document.querySelector('.uploading').style.display = "block";
- }
+   document.querySelector('.uploading').style.display = "block";
+}
 
- function handleUpdateData() {
-    document.querySelector('.update').style.display = "block";
- }
+function handleUpdateData() {
+   document.querySelector('.update').style.display = "block";
+}
+
+function getFile() {
+
+   const nroSec = document.querySelector('.input-secuencia').value;
+
+   fetch(`http://localhost:5000/data/secuencia?nroSec=${nroSec}`)
+      .then((response) => response.json())
+      .then(json => {
+
+         if (json) {
+            document.querySelector('.mensaje-secuencia').classList.add('mensaje-none');
+            return downloadTextFile(JSON.stringify(json), `${nroSec}.json`);
+         }
+         noExisteSecuencia(nroSec);
+      })
+      .catch(err => console.log('Solicitud fallida', err));
+
+}
+
+
+function downloadTextFile(text, name) {
+   const a = document.createElement('a');
+   const type = name.split(".").pop();
+   a.href = URL.createObjectURL(new Blob([text], { type: `text/${type === "txt" ? "plain" : type}` }));
+   a.download = name;
+   a.click();
+}
+
+function noExisteSecuencia(nroSec) {
+
+   const parrafo = document.querySelector('.mensaje-secuencia')
+   parrafo.classList.remove('mensaje-none');
+
+   if (nroSec) {
+      parrafo.innerHTML = `El N° de secuencia ${nroSec} no existe`;
+   } else {
+      parrafo.innerHTML = `Ingrese un N° de secuencia`;
+   }
+
+}
+
+const input = document.querySelector('.input-secuencia');
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function (event) {
+   // If the user presses the "Enter" key on the keyboard
+   if (event.key === "Enter") {
+      document.querySelector('.btn-secuencia').focus()
+   }
+})
